59c821b9f1978806bb8116d757db7f143a31f909
Seong Kyu Choi <seongcho@student.42seoul.kr>
2022-12-16 23:41:47 +0900

    updated

--- avaj-launcher/kr/ftseoul/avaj/simulator/AircraftFactory.java
+++ avaj-launcher/kr/ftseoul/avaj/simulator/AircraftFactory.java
@@ -2,6 +2,12 @@ package kr.ftseoul.avaj.simulator;
 
 public class AircraftFactory {
   public Flyable newAircraft(String type, String name, int longitude, int latitude, int height) {
+    if (height > Coordinates.MAX_HEIGHT) {
+      height = Coordinates.MAX_HEIGHT;
+    } else if (height < Coordinates.MIN_HEIGHT) {
+      height = Coordinates.MIN_HEIGHT;
+    }
+
     Coordinates coordinates = new Coordinates(longitude, latitude, height);
 
     if (type.equalsIgnoreCase(AircraftType.HELICOPTER.toString()))
--- avaj-launcher/kr/ftseoul/avaj/simulator/Simulator.java
+++ avaj-launcher/kr/ftseoul/avaj/simulator/Simulator.java
@@ -2,6 +2,12 @@ package kr.ftseoul.avaj.simulator;
 
 public class Simulator {
   public static void main(String[] args) {
+
+    doSimulationWithoutFile();
+
+  }
+
+  public static void doSimulationWithoutFile() {
     WeatherTower tower = new WeatherTower();
     //System.out.println(tower);
 
@@ -9,7 +15,6 @@ public class Simulator {
 
     Flyable b1 = factory.newAircraft("Baloon", "B1", 2, 3, 20);
     b1.registerTower(tower);
-
     Flyable b2 = factory.newAircraft("Baloon", "B2", 1, 8, 66);
     b2.registerTower(tower);
 
@@ -18,6 +23,19 @@ public class Simulator {
 
     Helicopter h1 = new Helicopter("H1", new Coordinates(654, 33, 20));
     h1.registerTower(tower);
+    Helicopter h2 = new Helicopter("H2", new Coordinates(22, 33, 44));
+    h2.registerTower(tower);
+    Helicopter h3 = new Helicopter("H3", new Coordinates(98, 68, 99));
+    h3.registerTower(tower);
+
+    Flyable b3 = factory.newAircraft("Baloon", "B3", 102, 22, 34);
+    b3.registerTower(tower);
+
+    Flyable j2 = factory.newAircraft("JetPlane", "J2", 11, 99, 768);
+    j2.registerTower(tower);
+
+    Helicopter h4 = new Helicopter("H4", new Coordinates(223, 23, 54));
+    h4.registerTower(tower);
 
     for (int i = 0; i < 25; i++) {
       tower.conditionsChanged();
@@ -33,7 +51,6 @@ public class Simulator {
 
     //System.out.println(WeatherType.valueOf("SUN").ordinal());
     //System.out.println(WeatherType.values()[1]);
-
 
   }
 }
