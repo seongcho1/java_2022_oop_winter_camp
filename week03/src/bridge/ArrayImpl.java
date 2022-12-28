a37a7abc17ef5e4aa7c0ea49ce1f422a8b7eb974
seongcho <indivisibilibus@gmail.com>
2022-12-28 17:59:09 +0900

    init

--- week03/src/bridge/ArrayImpl.java
+++ week03/src/bridge/ArrayImpl.java
@@ -1,5 +1,35 @@
 package bridge;
 
-public class ArrayImpl implements AbstractList {
+public class ArrayImpl<T> implements AbstractList<T> {
+
+  @Override
+  public void addElement(T obj) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public T deleteElement(int i) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public T getElement(int i) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public int getElementSize() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public int insertElement(T obj, int i) {
+    // TODO Auto-generated method stub
+    return 0;
+  }
 
 }
